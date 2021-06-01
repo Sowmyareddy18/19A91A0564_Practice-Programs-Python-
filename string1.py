1a86e3bc36bfe53f085938229d4788a246d3c8b7
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-01 15:37:06 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,26 @@
+name='Python'
+print(name)
+name='Python programming'
+print(name[1:6:2])
+print(name[-1])
+print(name[-2])
+#slicing using negative indexing
+print(name[-5:-2])
+#capitalizing
+S='devi priya'
+print(S.capitalize())
+print(S.upper())
+print(S.lower())
+print(S.isupper())
+print(S.islower())
+#output
+Python
+yhn
+g
+n
+mmi
+Devi priya
+DEVI PRIYA
+devi priya
+False
+True
