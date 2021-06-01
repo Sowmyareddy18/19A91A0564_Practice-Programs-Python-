d6eed040507254ad230d636e9f3f34fb398460e7
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-01 15:36:15 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,15 @@
+for s in 'cseA':
+    print(s)
+print('Iterating List')
+fruits={'Mango','Apple','orange'}
+for f in fruits:
+    print(f)
+
+#c
+#s
+#e
+#A
+#Iterating List
+#Apple
+#orange
+#Mango
