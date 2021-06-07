956b705f327e332e056ddf2bc215f65de5b5b838
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-07 12:32:47 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,12 @@
+set1={1,2,3,4,5,6}
+print("Elements in the set1 is: {}".format(set1))#Elements in the set1 is: {1, 2, 3, 4, 5, 6}
+#is the set allows duplicates?
+set2={10,20,30,40,40,60}
+print("Elements in the set2 is: {}".format(set2))#Elements in the set2 is: {20, 40, 10, 60, 30}
+#Data type
+print("Data type of set1",type(set1))#Data type of set1 <class 'set'>
+print("Data type of set1",type(set1))#Data type of set1 <class 'set'>
+print("Length of set1 is %d"%len(set1))#Length of set1 is 6
+print("min is %d and max is %d form the set2"%(min(set2),max(set2)))#min is 10 and max is 60 form the set2
+
+
