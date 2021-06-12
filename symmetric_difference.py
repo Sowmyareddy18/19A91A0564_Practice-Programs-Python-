da30afeb9021787a544ddb1e56d73657cc65deb5
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-12 11:38:29 +0530

    Create symmetric_difference.py

---
+++ symmetric_difference.py
@@ -0,0 +1,18 @@
+n = input()
+a = set(map(int, input().split()))
+n = input()
+b = set(map(int, input().split()))
+set1=sorted(a.symmetric_difference(b))
+for i in (set1):
+    print(i)
+'''
+INPUT:
+4
+2 4 5 9
+4
+2 4 11 12
+OUTPUT:
+5
+9
+11
+12
