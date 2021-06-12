7ab7436b82663d4a5800e178221ea0f55cc0066a
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-12 12:06:23 +0530

    Create set_intersection().py

---
+++ set_intersection().py
@@ -0,0 +1,14 @@
+n = input()
+a = set(map(int, input().split()))
+n = input()
+b = set(map(int, input().split()))
+print(len(a.intersection(b)))
+'''
+INPUT:
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+OUTPUT:
+5
+'''
