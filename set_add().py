c053b469ec774e86eaf01ad120c95991d3953ffd
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-06-14 14:06:49 +0530

    Create set_add().py

---
+++ set_add().py
@@ -0,0 +1,19 @@
+num = raw_input()
+country = set()
+for i in range(int(num)):
+    country.add(raw_input())
+print len(country)
+'''
+Input:
+Download
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+Output:
+5
+'''
