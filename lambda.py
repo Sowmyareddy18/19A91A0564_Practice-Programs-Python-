4d608bcdefe5df01106bc9355a147fb4abdaa1b0
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-01 15:36:44 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,9 @@
+#lambda function-A function is declared without any name-Anonymous function
+#syntax:- variable_name=lambda arguments:Expression
+
+res=lambda a,b,c:a+b*c
+#calling
+res1=res(10,2,3)
+print(res1)#16
+#calling
+print(res(10,2,3))#16
