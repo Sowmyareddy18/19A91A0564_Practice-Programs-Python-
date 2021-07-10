00c8d451324849f50a6f716fa54338148d5cc3cb
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-10 15:05:44 +0530

    Add files via upload

---
+++ listoperations_import_module.py
@@ -0,0 +1,11 @@
+from listoperations_module import list_sum,list_sort,string_list_conversion
+list1=[567,325,566,234,794]
+print(list_sum(list1))
+print(list_sort(list1))
+print(string_list_conversion("Hello-Python"))
+
+'''
+2486
+[234, 325, 566, 567, 794]
+['H', 'e', 'l', 'l', 'o', '-', 'P', 'y', 't', 'h', 'o', 'n']
+'''
