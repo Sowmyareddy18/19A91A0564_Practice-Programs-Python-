5ff2332841236e36f80e7f7d097f7fc14ac2ef12
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-12 15:40:47 +0530

    Add files via upload

---
+++ evenmod.py
@@ -0,0 +1,14 @@
+'''
+Python Modules
+Module-collection of classes,functions and variables
+Two types of modules
+1.User Defined module
+2.Predefined module
+'''
+def evencheck():
+    list1=[1,2,3,4,5,6,7,8,9,10]
+    #check even numbers
+    for i in list1:
+        if i%2==0:
+            print(i)
+#moduledemo.py
---
+++ moduledemo.py
@@ -0,0 +1,13 @@
+#use evencheck() from moduledemo.py
+#Access the module-import
+import evenmod
+#call-modulename.function
+evenmod.evencheck()
+
+'''
+2
+4
+6
+8
+10
+'''
