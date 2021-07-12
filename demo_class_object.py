86ae59de0b7e16ba8c9731d65e1c421d77bc4f3e
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-12 16:09:47 +0530

    Add files via upload

---
+++ demo_class_object.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def area(self,width,height):
+        return width*height
+#Create an Object for Rectange class
+Rect1=Rectangle()
+#calling the methode of Rectangle
+result=Rect1.area(3,5)
+print(result)#15
+#Create Rect2 object
+Rect2=Rectangle()
+#call
+print(Rect2.area(7,8))#56
+
+'''
+Creation of Object
+objectname=classname()
+#call the properies
+object.methodname(...)
+#variable
+objectname.variablename
+'''
