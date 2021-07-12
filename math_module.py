564c488b64d203af1e51f3906381785107a2a010
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-12 15:44:43 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,15 @@
+import math
+number=12.4
+res=math.ceil(number)
+print(res)
+res=math.floor(number)
+print("floor of {} is {}".format(number,res))
+res=math.cos(1)
+print(res)
+'''
+13
+floor of 12.4 is 12
+0.5403023058681398
+'''
+
+
