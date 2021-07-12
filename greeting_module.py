f3e7ad55a3d5b02c33a6f148d7436d2b4eeeb96b
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-07-12 15:02:02 +0530

    Add files via upload

---
+++ greeting_module.py
@@ -0,0 +1,4 @@
+#creating module
+def greetings(s):
+    print("Hello"+s)
+#greetingmodule1.py
---
+++ greetingmodule1.py
@@ -0,0 +1,5 @@
+#Access Module
+import greeting_module
+greeting_module.greetings("CSE-A")
+
+#HelloCSE-A
